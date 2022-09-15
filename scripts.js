6aeb0dba5cf1f57d1cdb709514fd00107b4db716
Jeru Balares <jerupaulbalares@gmail.com>
2022-09-15 13:56:49 -0700

    Added a createBox function before the addEventListener so the container is not blank or empty on start up

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+// Adds a specified number of divs with the class of box inside 
+// the container class.
 function createBox(num) {
     for (i = 0; i < num; i++) {
         const box = document.createElement('div');
@@ -7,6 +9,9 @@ function createBox(num) {
     }
 }
 
+// This function exists to create a default set up,
+// so the content is not blank
+createBox(16 * 16);
 
 const button = document.querySelector(".buttons");
 button.addEventListener('click', function (e) {
@@ -14,4 +19,6 @@ button.addEventListener('click', function (e) {
     const num = e.target.id * e.target.id
     createBox(num);
 });
+
+
 
